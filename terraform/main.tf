11834a02e5c1b72f5341e4396bf08c2c039f0924
Fatima Aalam <fatimaalam773@gmail.com>
2026-07-21 09:07:45 +0000

    Switch CI/CD to ECR via OIDC, sync Terraform state with hardened infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,3 +61,65 @@ resource "aws_ecr_repository" "inventory_app" {
     scan_on_push = true
   }
 }
+# --- GitHub OIDC Provider ---
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+# --- IAM Role assumed by GitHub Actions ---
+resource "aws_iam_role" "github_actions_ecr" {
+  name = "github-actions-ecr-push"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:FatimaAalam/inventory_app:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "github_actions_ecr_push" {
+  name = "ecr-push-policy"
+  role = aws_iam_role.github_actions_ecr.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "ECRAuth"
+        Effect = "Allow"
+        Action = "ecr:GetAuthorizationToken"
+        Resource = "*"
+      },
+      {
+        Sid    = "ECRPush"
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:BatchGetImage"
+        ]
+        Resource = aws_ecr_repository.inventory_app.arn
+      }
+    ]
+  })
+}
